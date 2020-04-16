f1b427d1c82c17b818639b0db56a8b6c3630b006
username <user@email.mail>
2020-04-16 09:51:22 +0900

    메인화면 좌측 메뉴 변경 적용

--- AcademyByEGov_3.9.0/src/main/java/egovframework/com/academy/index/IndexController.java
+++ AcademyByEGov_3.9.0/src/main/java/egovframework/com/academy/index/IndexController.java
@@ -29,9 +29,14 @@ package egovframework.com.academy.index;
  */
 
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.annotation.Resource;
+
+import egovframework.com.academy.menu.service.EgovMenuManageService;
+import egovframework.com.academy.menu.service.MenuManageVO;
 import egovframework.com.cmm.IncludedCompInfoVO;
 import egovframework.com.cmm.LoginVO;
 import egovframework.com.cmm.annotation.IncludedInfo;
@@ -45,6 +50,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -53,6 +59,11 @@ public class IndexController implements ApplicationContextAware, InitializingBea
 	private ApplicationContext applicationContext;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
+    /** EgovMenuManageService */
+	@Resource(name = "meunManageService")
+    private EgovMenuManageService menuManageService;
+
 
 	public void afterPropertiesSet() throws Exception {}
 
@@ -87,76 +98,11 @@ public class IndexController implements ApplicationContextAware, InitializingBea
 	}
 
 	@RequestMapping("/EgovLeft.do")
-	public String setLeftMenu(ModelMap model) {
+	public String setLeftMenu(@ModelAttribute("menuManageVO") MenuManageVO menuManageVO, ModelMap model) throws Exception {
 
-		Map<Integer, IncludedCompInfoVO> map = new TreeMap<Integer, IncludedCompInfoVO>();
-		RequestMapping rmAnnotation;
-		IncludedInfo annotation;
-		IncludedCompInfoVO zooVO;
-
-		/*
-		 * EgovLoginController가 AOP Proxy되는 바람에 클래스를 reflection으로 가져올 수 없음
-		 */
-		try {
-			Class<?> loginController = Class.forName("egovframework.com.academy.login.web.EgovLoginController");
-			Method[] methods = loginController.getMethods();
-			for (int i = 0; i < methods.length; i++) {
-				annotation = methods[i].getAnnotation(IncludedInfo.class);
-
-				if (annotation != null) {
-					LOGGER.debug("Found @IncludedInfo Method : {}", methods[i]);
-					zooVO = new IncludedCompInfoVO();
-					zooVO.setName(annotation.name());
-					zooVO.setOrder(annotation.order());
-					zooVO.setGid(annotation.gid());
-
-					rmAnnotation = methods[i].getAnnotation(RequestMapping.class);
-					if ("".equals(annotation.listUrl()) && rmAnnotation != null) {
-						zooVO.setListUrl(rmAnnotation.value()[0]);
-					} else {
-						zooVO.setListUrl(annotation.listUrl());
-					}
-					map.put(zooVO.getOrder(), zooVO);
-				}
-			}
-		} catch (ClassNotFoundException e) {
-			LOGGER.error("No egovframework.com.academy.login.web.EgovLoginController!!");
-		}
-		/* 여기까지 AOP Proxy로 인한 코드 */
-
-		/*@Controller Annotation 처리된 클래스를 모두 찾는다.*/
-		Map<String, Object> myZoos = applicationContext.getBeansWithAnnotation(Controller.class);
-		LOGGER.debug("How many Controllers : ", myZoos.size());
-		for (final Object myZoo : myZoos.values()) {
-			Class<? extends Object> zooClass = myZoo.getClass();
-
-			Method[] methods = zooClass.getMethods();
-			LOGGER.debug("Controller Detected {}", zooClass);
-			for (int i = 0; i < methods.length; i++) {
-				annotation = methods[i].getAnnotation(IncludedInfo.class);
-
-				if (annotation != null) {
-					//LOG.debug("Found @IncludedInfo Method : " + methods[i] );
-					zooVO = new IncludedCompInfoVO();
-					zooVO.setName(annotation.name());
-					zooVO.setOrder(annotation.order());
-					zooVO.setGid(annotation.gid());
-					/*
-					 * 목록형 조회를 위한 url 매핑은 @IncludedInfo나 @RequestMapping에서 가져온다
-					 */
-					rmAnnotation = methods[i].getAnnotation(RequestMapping.class);
-					if ("".equals(annotation.listUrl())) {
-						zooVO.setListUrl(rmAnnotation.value()[0]);
-					} else {
-						zooVO.setListUrl(annotation.listUrl());
-					}
-
-					map.put(zooVO.getOrder(), zooVO);
-				}
-			}
-		}
-
-		model.addAttribute("resultList", map.values());
+    	List<?> list_menulist = menuManageService.selectMainMenuLeft(menuManageVO);
+		
+		model.addAttribute("resultList", list_menulist);
 		
 		LOGGER.debug("EgovComIndexController index is called ");
 
