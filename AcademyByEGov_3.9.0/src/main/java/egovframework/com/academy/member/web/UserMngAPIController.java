a9cc58f45075e62265d5140862ef2a5f2b5ed581
username <user@email.mail>
2020-04-20 08:36:13 +0900

    회원정보 JSON API 추가

---
+++ AcademyByEGov_3.9.0/src/main/java/egovframework/com/academy/member/web/UserMngAPIController.java
@@ -0,0 +1,122 @@
+package egovframework.com.academy.member.web;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
+import egovframework.com.academy.member.service.EgovUserManageService;
+import egovframework.com.academy.member.service.UserDefaultVO;
+import egovframework.com.api.CORSFilter;
+import egovframework.com.cmm.service.EgovCmmUseService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
+/**
+ * 업무사용자관련 요청을  비지니스 클래스로 전달하고 처리된결과를  해당   웹 화면으로 전달하는  Controller를 정의한다
+ * @author 공통서비스 개발팀 조재영
+ * @since 2009.04.10
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.04.10  조재영          최초 생성
+ *   2011.08.26	 정진오			IncludedInfo annotation 추가
+ *   2014.12.08	 이기하			암호화방식 변경(EgovFileScrty.encryptPassword)
+ *   2015.06.16	 조정국			수정시 유효성체크 후 에러발생 시 목록으로 이동하여 에러메시지 표시
+ *   2015.06.19	 조정국			미인증 사용자에 대한 보안처리 기준 수정 (!isAuthenticated)
+ *   2017.07.21  장동한 			로그인인증제한 작업
+ *  2020.03.00	rainend			JSON API
+ * </pre>
+ */
+
+@Controller
+public class UserMngAPIController extends CORSFilter {
+
+	/** userManageService */
+	@Resource(name = "userManageService")
+	private EgovUserManageService userManageService;
+
+	/** cmmUseService */
+	@Resource(name = "EgovCmmUseService")
+	private EgovCmmUseService cmmUseService;
+
+	/** EgovPropertyService */
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertiesService;
+
+	/** DefaultBeanValidator beanValidator */
+	@Autowired
+	private DefaultBeanValidator beanValidator;
+
+	/**
+	 * 사용자목록을 조회한다. (pageing)
+	 * @param userSearchVO 검색조건정보
+	 * @param model 화면모델
+	 * @return /member/EgovUserManage
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/api/member/list")
+	@ResponseBody
+	public ModelAndView selectUserList(@ModelAttribute("userSearchVO") UserDefaultVO userSearchVO) throws Exception {
+
+    	ModelAndView modelAndView = new ModelAndView();
+    	modelAndView.setViewName("jsonView");
+
+		/** EgovPropertyService */
+		userSearchVO.setPageUnit(propertiesService.getInt("pageUnit"));
+		userSearchVO.setPageSize(propertiesService.getInt("pageSize"));
+
+		/** pageing */
+		PaginationInfo paginationInfo = new PaginationInfo();
+		paginationInfo.setCurrentPageNo(userSearchVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(userSearchVO.getPageUnit());
+		paginationInfo.setPageSize(userSearchVO.getPageSize());
+
+		userSearchVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		userSearchVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		userSearchVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+		List<?> userList = userManageService.selectUserList(userSearchVO);
+		modelAndView.addObject(userList);
+
+		return modelAndView;
+	}
+
+	/**
+	 * 입력한 사용자아이디의 중복여부를 체크하여 사용가능여부를 확인
+	 * @param commandMap 파라메터전달용 commandMap
+	 * @param model 화면모델
+	 * @return member/EgovIdDplctCnfirm
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/api/member/EgovIdDplctCnfirmAjax.do")
+	public ModelAndView checkIdDplctAjax(@RequestParam Map<String, Object> commandMap) throws Exception {
+
+    	ModelAndView modelAndView = new ModelAndView();
+    	modelAndView.setViewName("jsonView");
+
+		String checkId = (String) commandMap.get("checkId");
+		//checkId = new String(checkId.getBytes("ISO-8859-1"), "UTF-8");
+
+		int usedCnt = userManageService.checkIdDplct(checkId);
+		modelAndView.addObject("usedCnt", usedCnt);
+		modelAndView.addObject("checkId", checkId);
+
+		return modelAndView;
+	}
+
+}
