f7575bff267ec06e4815f24f1ed882cc9f67e3d9
username <user@email.mail>
2020-05-05 18:15:27 +0900

    시험정보 API 등록

---
+++ AcademyByEGov_3.9.0/src/main/java/egovframework/com/academy/exam/web/ExamApiController.java
@@ -0,0 +1,83 @@
+package egovframework.com.academy.exam.web;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import egovframework.com.academy.exam.service.ExamManageService;
+import egovframework.com.academy.exam.service.ExamVO;
+import egovframework.com.api.CORSFilter;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
+/**
+ * 시험(과목)정보 처리를  비지니스 클래스로 전달하고 처리된결과를  해당 웹 화면으로 전달하는 Controller를 정의한다
+ * @author rainend
+ * @version 1.0
+ * @see
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *   수정일      			수정자           수정내용
+ *  ----------------    --------------    ---------------------------
+ *  2020.04.00  			rainend          최초 생성
+ * </pre>
+ */
+
+@Controller
+public class ExamApiController extends CORSFilter {
+
+	@Resource(name = "examManageService")
+	private ExamManageService examManageService;
+
+	/** EgovMessageSource */
+	@Resource(name = "egovMessageSource")
+	EgovMessageSource egovMessageSource;
+
+	/** EgovPropertyService */
+	@Resource(name = "propertiesService")
+    protected EgovPropertyService propertyService;
+
+	/**
+	 * 시험 목록화면 이동
+	 * @return String
+	 * @exception Exception
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/api/exam/list", method= RequestMethod.GET)
+	public ModelAndView ExamList(@ModelAttribute("ExamVO") ExamVO ExamVO) throws Exception {
+
+    	ModelAndView modelAndView = new ModelAndView();
+    	modelAndView.setViewName("jsonView");
+
+		ExamVO.setPageUnit(propertyService.getInt("pageUnit"));
+		ExamVO.setPageSize(propertyService.getInt("pageSize"));
+
+		/** paging */
+		PaginationInfo paginationInfo = new PaginationInfo();
+		paginationInfo.setCurrentPageNo(ExamVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(ExamVO.getPageUnit());
+		paginationInfo.setPageSize(ExamVO.getPageSize());
+
+		ExamVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		ExamVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		ExamVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+		List<?> examList = (List<?>) examManageService.selectExamList(ExamVO);
+		modelAndView.addObject(examList);
+		
+		int totCnt = examManageService.selectExamListTotCnt(ExamVO);
+		paginationInfo.setTotalRecordCount(totCnt);
+		modelAndView.addObject(paginationInfo);
+		
+		return modelAndView;
+	}
+
+}
